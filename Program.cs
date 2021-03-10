b452dd107eeedcc13d33ef8607f8732cb6bbc231
parsa mahmoudy <43126637+parsa011@users.noreply.github.com>
2021-03-10 11:19:06 +0330

    add file :D

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names;
+            Console.WriteLine("How much name you want ?");
+            int namesCount;
+            if (int.TryParse(Console.ReadLine(),out namesCount))
+            {
+                names = new string[namesCount];
+                int biggest = 0;
+                for (var i = 0; i < namesCount; i++)
+                {
+                    Console.Write("> ");
+                    var line = Console.ReadLine();
+                    biggest = line.Length > biggest ? line.Length : biggest;
+                    names[i] = line;
+                }
+                for (var i = 0; i < names.Length; i++)
+                {
+                    var current = names[i];
+                    var need = biggest - current.Length;
+                    var finallSpaces = GiveMeSpace(need);
+                    names[i] = current + finallSpaces;
+                }
+                int neededLoopTimes = biggest - 1;
+                Console.ForegroundColor = ConsoleColor.Red;
+                while (neededLoopTimes >= 0)
+                {
+                    for (var i = 0; i < names.Length; i++)
+                    {
+                        Console.Write(GiveMeSpace(10));
+                        if (names[i][neededLoopTimes] == ' ')
+                            Console.Write(" ");
+                        else
+                            Console.Write("*");
+                    }
+                    neededLoopTimes--;
+                    Console.WriteLine();
+                }
+                Console.ResetColor();
+                neededLoopTimes = 0;
+                while (neededLoopTimes < biggest)
+                {
+                    for (var index = 0; index < names.Length; index++)
+                    {
+                        Console.Write($"{GiveMeSpace(10) +  GiveMeSpace(neededLoopTimes) + names[index][neededLoopTimes].ToString() } ");
+                    }
+                    neededLoopTimes++;
+                    Console.WriteLine();
+                }
+            }
+            else
+                Console.WriteLine("please enter valid in32 number");
+            
+        }
+
+        public static string GiveMeSpace(int count)
+        {
+            var finallSpaces = "";
+            for (var j = 0; j < count; j++)
+            {
+                finallSpaces += " ";
+            }
+            return finallSpaces;
+        }
+    }
+}
